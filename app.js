71a2a504c2bcb16abba1f1dcf9ade5213237dcc8
Ajay <ajay.summer2@gmail.com>
2020-10-11 18:20:56 +0100

    Randomize board

--- app.js
+++ app.js
@@ -99,5 +99,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    shuffleArray(cardArray)
     createBoard()
 })
