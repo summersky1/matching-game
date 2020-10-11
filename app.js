bd0401328592df4dd09b77bb72d9e005b9278fc3
Ajay <ajay.summer2@gmail.com>
2020-10-11 18:05:52 +0100

    Add basic game logic

--- app.js
+++ app.js
@@ -51,13 +51,45 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     const grid = document.querySelector('.grid')
+    let chosenCardNames = []
+    let chosenCardIds = []
+    let clearedCards = []
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             let card = document.createElement('img')
             card.setAttribute('src', 'images/blank.png')
             card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
             grid.appendChild(card)
+        }
+    }
+
+    function checkForMatch() {
+        let cards = document.querySelectorAll('img')
+        const firstCardId = chosenCardIds[0]
+        const secondCardId = chosenCardIds[1]
+        if (chosenCardNames[0] === chosenCardNames[1]) {
+            alert('You found a match')
+            cards[firstCardId].setAttribute('src', 'images/white.png')
+            cards[secondCardId].setAttribute('src', 'images/white.png')
+            clearedCards.push(chosenCardNames)
+        } else {
+            cards[firstCardId].setAttribute('src', 'images/blank.png')
+            cards[secondCardId].setAttribute('src', 'images/blank.png')
+            alert('Sorry, try again')
+        }
+        chosenCardNames = []
+        chosenCardIds = []
+    }
+
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        chosenCardNames.push(cardArray[cardId].name)
+        chosenCardIds.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (chosenCardNames.length === 2) {
+            setTimeout(checkForMatch, 500)
         }
     }
 
